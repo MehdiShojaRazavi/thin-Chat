00654e405927996150ac3b4381a6f924f24a6007
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-09-14 20:54:10 +0300

    add startServer

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -8,6 +8,8 @@ import cookerSession from 'cookie-session';
 import HTTP_STATUS from 'http-status-codes';
 import 'express-async-errors';
 
+const SERVER_PORT = 5000;
+
 export class ChattyServer{
   private app: Application;
   constructor (app: Application){
@@ -48,8 +50,20 @@ export class ChattyServer{
 
   private routeMiddleware(app: Application): void {}
   private globalErrorHandler(app: Application): void {}
-  private startServer(app: Application): void {}
+  private async startServer(app: Application): Promise<void> {
+    try{
+      const httpServer: http.Server = new http.Server(app);
+      this.startHttpServer(httpServer);
+    } catch(error) {
+      console.log(error);
+    }
+  }
   private createSocketIO(httpServer: http.Server): void {}
-  private startHttpServer(httpServer: http.Server): void {}
+
+  private startHttpServer(httpServer: http.Server): void {
+    httpServer.listen(SERVER_PORT, () => {
+      console.log(`Server running on port ${SERVER_PORT}`)
+    })
+  }
 
 }
