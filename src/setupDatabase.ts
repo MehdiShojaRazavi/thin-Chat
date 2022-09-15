521e163d22a42d044903e502da452fe5533a0b10
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-09-15 21:56:08 +0300

    add config

--- src/setupDatabase.ts
+++ src/setupDatabase.ts
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
+import { config } from './config';
 
 export default () => {
   const connect = () => {
-    mongoose.connect('mongodb://localhost:27017/thinChat-Backend')
+    mongoose.connect(config.DATABASE_URL!)
       .then(() => {
         console.log('Sucessfully connect to mongoDB');
       })
--- src/setupServer.ts
+++ src/setupServer.ts
@@ -7,6 +7,7 @@ import helmet from 'helmet';
 import cookerSession from 'cookie-session';
 import HTTP_STATUS from 'http-status-codes';
 import 'express-async-errors';
+import { config } from './config';
 
 const SERVER_PORT = 5000;
 
@@ -27,14 +28,14 @@ export class ChattyServer{
   private securityMiddleware(app: Application): void {
     app.use(cookerSession({
       name: 'session',
-      keys: ['test1', 'test2'],
+      keys: [config.SECRET_KEY_ONE!, config.SECRET_KEY_TWO!],
       maxAge: 24 * 7 * 3600000,
-      secure: false
+      secure: config.NODE_ENV !== 'development'
     }));
     app.use(hpp());
     app.use(helmet());
     app.use(cors({
-      origin: '*',
+      origin: config.CLIENT_URL,
       credentials: true,
       optionsSuccessStatus: 200,
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
